0f79b420212bd650bbc6ecf2590d8b48fe9d8449
Kevin Ji <kevin.ji@outlook.com>
2015-01-17 10:37:37 -0500

    app: Fix Yelp

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/yelp', express.static(__dirname + '/yelp'));
+app.use('/yelp', function() { express.static(__dirname + '/yelp'); });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

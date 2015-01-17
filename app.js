cd3b6742e4947d5f6376fe8fcb91e9868af9e3a7
Kevin Ji <kevin.ji@outlook.com>
2015-01-17 12:56:04 -0500

    Add MongoDB packages

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');

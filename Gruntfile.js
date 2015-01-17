cf7feb46f19d5710250be6afacb04b088568067b
Kevin Ji <kevin.ji@outlook.com>
2015-01-17 07:08:20 -0500

    Add basic grunt support

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+
+		shell: {
+			firebase: {
+				command: "firebase deploy"
+			}
+		}
+	});
+
+	grunt.loadNpmTasks("grunt-shell");
+
+	grunt.registerTask("firebase", ["shell:firebase"]);
+};
